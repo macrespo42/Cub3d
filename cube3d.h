eff359d24a24f286ea4f779d091a2412c227a230
macrespo <maxime.crespopro@gmail.com>
2019-12-03 14:30:20 +0100

    close programm properly when with mouse

--- cube3d.h
+++ cube3d.h
@@ -6,7 +6,7 @@
 /*   By: macrespo <macrespo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/02 14:57:38 by macrespo          #+#    #+#             */
-/*   Updated: 2019/12/02 18:30:09 by macrespo         ###   ########.fr       */
+/*   Updated: 2019/12/03 14:21:29 by macrespo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,10 @@
 # include <string.h>
 # include <math.h>
 # include <mlx.h>
+# define NAME "cube3d"
+# define SKY 0x00b4c4
+# define FLOOR 0x333333
+# define CUBE 0xc5002b
 
 typedef struct		s_img
 {
--- main.c
+++ main.c
@@ -6,25 +6,28 @@
 /*   By: macrespo <macrespo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/28 15:18:42 by macrespo          #+#    #+#             */
-/*   Updated: 2019/12/03 12:16:23 by macrespo         ###   ########.fr       */
+/*   Updated: 2019/12/03 14:28:50 by macrespo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cube3d.h"
-
 t_data		g_data;
+
+int close_window(void)
+{
+	mlx_destroy_window(g_data.mlx_ptr, g_data.mlx_win);
+	exit(0);
+	return (0);
+}
 
 int		key_hook(int key, void *arg)
 {
 	(void)arg;
 	printf("%d\n", key);
 	if (key == 53)
-	{
-		mlx_destroy_window(g_data.mlx_ptr, g_data.mlx_win);
-		exit(0);
-	}
+		close_window();
 	else if (key == 0)
-		mlx_string_put(g_data.mlx_ptr, g_data.mlx_win, 960, 540, 0xFF1493, "cube3d");
+		mlx_string_put(g_data.mlx_ptr, g_data.mlx_win, 360, 240, SKY, NAME);
 	return (key);
 }
 
@@ -32,9 +35,10 @@ int		main(void)
 {
 	if ((g_data.mlx_ptr = mlx_init()) == NULL)
 		return (EXIT_FAILURE);
-	if (!(g_data.mlx_win = mlx_new_window(g_data.mlx_ptr, 1920, 1080, "cube3d")))
+	if (!(g_data.mlx_win = mlx_new_window(g_data.mlx_ptr, 720, 480, NAME)))
 		return (EXIT_FAILURE);
 	mlx_key_hook(g_data.mlx_win, key_hook, NULL);
+	mlx_hook(g_data.mlx_win, 17, 0, close_window, NULL);
 	mlx_loop(g_data.mlx_ptr);
 	return (EXIT_SUCCESS);
 }
