887a78809f21d3464083ea0da5ed29130340b609
macrespo <maxime.crespopro@gmail.com>
2019-12-09 10:18:26 +0100

    parsing window resolution OK

--- parser/get_resolution.c
+++ parser/get_resolution.c
@@ -6,7 +6,7 @@
 /*   By: macrespo <macrespo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/05 22:35:41 by macrespo          #+#    #+#             */
-/*   Updated: 2019/12/07 18:21:28 by macrespo         ###   ########.fr       */
+/*   Updated: 2019/12/09 10:16:00 by macrespo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,16 @@ int		get_resolution(const char *s, int *pos)
 	int		res;
 
 	res = 0;
+	if (*pos == 0 || (s[*pos] >= '0' && s[*pos] <= '9'))
+		(*pos)++;
 	while (s[*pos] == '\t' || s[*pos] == '\n' || s[*pos] == '\r' || s[*pos] == '\v'
 	|| s[*pos] == '\f' || s[*pos] == ' ')
 		*pos += 1;
 	while (s[*pos] >= '0' && s[*pos] <= '9')
 	{
 		res = res * 10 + s[*pos] - '0';
-		*pos++;
+		(*pos)++;
 	}
+	(*pos)--;
 	return (res);
 }
--- parser/main.c
+++ parser/main.c
@@ -6,7 +6,7 @@
 /*   By: macrespo <macrespo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/07 09:46:12 by macrespo          #+#    #+#             */
-/*   Updated: 2019/12/07 18:20:49 by macrespo         ###   ########.fr       */
+/*   Updated: 2019/12/09 10:12:24 by macrespo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,18 @@
 
 t_data		g_data;
 
+void	print_data()
+{
+	printf("RESOLUTION : %d x %d \n", g_data.x, g_data.y);
+}
+
 int		main(int ac, char **av)
 {
 	if (ac == 2 || ac == 3)
+	{
 		parser(av[1]);
+		print_data();
+	}
 	else
 		write(1, "Error\n", 6);
 }
