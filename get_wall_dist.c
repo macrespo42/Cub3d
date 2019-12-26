54218607cb8d2e1d82c504d27760976bfbaf5d5a
macrespo <maxime.crespopro@gmail.com>
2019-12-26 18:08:53 +0100

    new wall dist conditions to debug

--- get_wall_dist.c
+++ get_wall_dist.c
@@ -6,7 +6,7 @@
 /*   By: macrespo <macrespo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/20 15:13:08 by macrespo          #+#    #+#             */
-/*   Updated: 2019/12/23 11:46:21 by macrespo         ###   ########.fr       */
+/*   Updated: 2019/12/26 18:08:07 by macrespo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,13 +72,21 @@ t_hit				get_wall_dist(t_cam cam)
 {
 	t_hit	hit;
 
-	if (cam.dir == 'N')
+	if (cam.d_y < 0 && cam.d_x < cos(-M_PI/4) && cam.d_x > cos(M_PI/4))
 		hit = get_wall_dist_north(cam);
-	if (cam.dir == 'S')
+	if (cam.d_y > 0 && cam.d_x < cos(-M_PI/4) && cam.d_x > cos(M_PI/4))
 		hit = get_wall_dist_south(cam);
-	if (cam.dir == 'E')
+	if (cam.d_x > 0 && cam.d_y < cos(-M_PI/4) && cam.d_x > cos(M_PI/4))
 		hit = get_wall_dist_east(cam);
-	if (cam.dir == 'W')
+	if (cam.d_x < 0 && cam.d_y < cos(-M_PI/4) && cam.d_x > cos(M_PI/4))
 		hit = get_wall_dist_west(cam);
+	// if (cam.d_y < 0 && (cam.d_x < cos(-M_PI/4) && cam.d_x > cos(M_PI/4)))
+	// 	hit = get_wall_dist_north(cam);
+	// if (cam.d_y > 0 && (cam.d_x < cos(-M_PI/4) && cam.d_x > cos(M_PI/4)))
+	// 	hit = get_wall_dist_south(cam);
+	// if (cam.d_x > 0 && (cam.d_y < cos(-M_PI/4) && cam.d_x > cos(M_PI/4)))
+	// 	hit = get_wall_dist_east(cam);
+	// if (cam.d_x < 0 && (cam.d_y < cos(-M_PI/4) && cam.d_x > cos(M_PI/4)))
+	// 	hit = get_wall_dist_west(cam);
 	return (hit);
 }
