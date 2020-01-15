2d619d33e3e2f85dbd2fbfd181213b421afff8cc
macrespo <maxime.crespopro@gmail.com>
2020-01-15 17:16:24 +0100

    adjust move

--- move.c
+++ move.c
@@ -6,7 +6,7 @@
 /*   By: macrespo <macrespo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/15 15:05:12 by macrespo          #+#    #+#             */
-/*   Updated: 2020/01/15 17:14:17 by macrespo         ###   ########.fr       */
+/*   Updated: 2020/01/15 17:16:08 by macrespo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,18 @@ void	rotate(int event, t_draw *d)
 {
 	double	old_dx;
 	double	old_px;
-	double	t;
+	double	speed;
 
-	t = 0.0;
+	speed = 0.0;
 	if (event == RIGHT)
-		t = TURN;
+		speed = TURN;
 	else if (event == LEFT)
-		t = -TURN;
+		speed = -TURN;
 	old_dx = d->cam.d_x;
 	old_px = d->cam.p_x;
-	d->cam.d_x = d->cam.d_x * cos(t) - d->cam.d_y * sin(t);
-	d->cam.d_y = old_dx * sin(t) + d->cam.d_y * cos(t);
-	d->cam.p_x = d->cam.p_x * cos(t) - d->cam.p_y * sin(t);
-	d->cam.p_y = old_px * sin(t) + d->cam.p_y * cos(t);
+	d->cam.d_x = d->cam.d_x * cos(speed) - d->cam.d_y * sin(speed);
+	d->cam.d_y = old_dx * sin(speed) + d->cam.d_y * cos(speed);
+	d->cam.p_x = d->cam.p_x * cos(speed) - d->cam.p_y * sin(speed);
+	d->cam.p_y = old_px * sin(speed) + d->cam.p_y * cos(speed);
 	draw(d);
 }
