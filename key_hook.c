d2857047c65977831f832d0a57da3d7fe9e9b364
Maxime CRESPO <macrespo@e1r8p15.42.fr>
2020-01-23 13:19:10 +0100

    fix leaks on texture parsing

--- key_hook.c
+++ key_hook.c
@@ -6,7 +6,7 @@
 /*   By: macrespo <macrespo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/04 15:14:24 by macrespo          #+#    #+#             */
-/*   Updated: 2020/01/17 14:56:02 by macrespo         ###   ########.fr       */
+/*   Updated: 2020/01/23 13:16:37 by macrespo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,24 @@
 
 int		close_window(void)
 {
+	int		y;
+
 	mlx_destroy_window(g_mlx.ptr, g_mlx.win);
+	y = 0;
+	while (y < 64)
+	{
+		free(g_data.no[y]);
+		free(g_data.so[y]);
+		free(g_data.we[y]);
+		free(g_data.ea[y]);
+		free(g_data.s[y]);
+		y++;
+	}
+	free((void*)g_data.no);
+	free((void*)g_data.so);
+	free((void*)g_data.we);
+	free((void*)g_data.ea);
+	free((void*)g_data.s);
 	free(g_mlx.ptr);
 	exit(0);
 	return (0);
