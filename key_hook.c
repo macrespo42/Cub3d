14aa5cc9a70b35c49ee1f000be2fab2ca0d8f87c
macrespo <maxime.crespopro@gmail.com>
2020-02-06 12:48:37 +0100

    del debug while

--- key_hook.c
+++ key_hook.c
@@ -6,7 +6,7 @@
 /*   By: macrespo <macrespo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/04 15:14:24 by macrespo          #+#    #+#             */
-/*   Updated: 2020/02/06 12:14:36 by macrespo         ###   ########.fr       */
+/*   Updated: 2020/02/06 12:48:22 by macrespo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,8 +56,6 @@ int				close_window(int error)
 	}
 	free_datas();
 	ft_memdel(g_mlx.ptr);
-	while (1)
-		;
 	exit(error);
 	return (0);
 }
