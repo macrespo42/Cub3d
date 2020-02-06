95b2c5444a0cd13ff18bceec9f92ba3c87c58471
macrespo <maxime.crespopro@gmail.com>
2020-02-06 22:15:34 +0100

    correct bmp with right chmod

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: macrespo <macrespo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/02 14:57:38 by macrespo          #+#    #+#             */
-/*   Updated: 2020/02/06 15:59:04 by macrespo         ###   ########.fr       */
+/*   Updated: 2020/02/06 21:51:20 by macrespo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@
 # define VOID_COLOR 0xFF000000
 # define HEADER_SIZE 14
 # define HEADER_INFO_SIZE 40
+# define COLOR_BUFFER 4
 
 typedef struct		s_img
 {
@@ -128,6 +129,7 @@ void				ray(t_draw *d);
 void				rotate(int event, t_draw *d);
 void				sort_sprites(t_draw *d);
 void				vertical_move(int event, t_draw *infos);
+void				write_img(int fd, t_draw *d);
 void				write_infos_header(int fd);
 void				write_header(int fd);
 extern t_mlx		g_mlx;
--- save.c
+++ save.c
@@ -6,11 +6,35 @@
 /*   By: macrespo <macrespo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/05 15:00:45 by macrespo          #+#    #+#             */
-/*   Updated: 2020/02/06 16:08:32 by macrespo         ###   ########.fr       */
+/*   Updated: 2020/02/06 22:00:37 by macrespo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+void	write_img(int fd, t_draw *d)
+{
+	unsigned char	buffer[COLOR_BUFFER];
+	int				i;
+	int				j;
+
+	i = g_data.x * (g_data.y - 1);
+	while (i >= 0)
+	{
+		j = 0;
+		while (j < g_data.x)
+		{
+			buffer[0] = (unsigned char)(d->img.grid[i] % 256);
+			buffer[1] = (unsigned char)(d->img.grid[i] / 256 % 256);
+			buffer[2] = (unsigned char)(d->img.grid[i] / 256 / 256 % 256);
+			buffer[3] = (unsigned char)0;
+			write(fd, buffer, COLOR_BUFFER);
+			j++;
+			i++;
+		}
+		i -= 2 * g_data.x;
+	}
+}
 
 void	write_infos_header(int fd)
 {
@@ -58,10 +82,15 @@ void	create_bmp(t_draw *d)
 {
 	int		fd;
 
-	fd = open("save.bmp", O_CREAT | O_WRONLY);
+	fd = open("save.bmp", O_CREAT | O_WRONLY, S_IRUSR);
+	if (fd == -1)
+		perror("Error, while opening file");
 	write_header(fd);
 	write_infos_header(fd);
+	write_img(fd, d);
 	ft_memdel(d->img.ptr);
 	ft_memdel(d->img.grid);
+	if (close(fd) == -1)
+		perror("Error, while closing file");
 	close_window(0);
 }
